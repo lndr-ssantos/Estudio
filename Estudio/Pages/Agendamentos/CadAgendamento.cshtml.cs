6248315b9f8119df50e94c998d6f89d1a47d95d2
Leandro Souza <l.souzadsantos@gmail.com>
2019-03-12 16:50:24 -0300

    Reorganização na busca de bandas e salas em CadAgendamento

--- Estudio/Pages/Agendamentos/CadAgendamento.cshtml.cs
+++ Estudio/Pages/Agendamentos/CadAgendamento.cshtml.cs
@@ -12,17 +12,9 @@ namespace Estudio.Pages.Agendamentos {
 	public class CadAgendamentoModel : PageModel {
 		private readonly EstudioContext _context;
 		//Lista de bandas para o select
-		public List<SelectListItem> Bandas => _context.Banda
-				.Select(x => new SelectListItem {
-					Value = x.IdBanda.ToString(),
-					Text = x.NomeBanda
-				}).ToList();
+		public List<SelectListItem> Bandas;
 		//Lista de salas para o select
-		public List<SelectListItem> Salas => _context.Sala
-			.Select(x => new SelectListItem {
-				Value = x.IdSala.ToString(),
-				Text = x.Nome
-			}).ToList();
+		public List<SelectListItem> Salas;
 
 		public CadAgendamentoModel(EstudioContext context) {
 			_context = context;
@@ -32,7 +24,17 @@ namespace Estudio.Pages.Agendamentos {
 		public Agendamento Agendamento { get; set; }
 
 		public void OnGet() {
+			Bandas = _context.Banda
+				.Select(x => new SelectListItem {
+					Value = x.IdBanda.ToString(),
+					Text = x.NomeBanda
+				}).ToList();
 
+			Salas = _context.Sala
+					.Select(x => new SelectListItem {
+						Value = x.IdSala.ToString(),
+						Text = x.Nome
+					}).ToList();
 		}
 
 		public async Task<IActionResult> OnPostAsync() {
